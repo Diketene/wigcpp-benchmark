53857a263ab39dee1fb24a15b96173dd01cb40d8
Diketene <liuhaotian0406@163.com>
2026-06-06 12:20:54 +0800

    modefied tests

--- src/correctness.cpp
+++ src/correctness.cpp
@@ -20,11 +20,11 @@ auto correctness_3j(int max_two_j) {
   wigcpp::ensure_global(2 * max_two_j, 3);
   std::vector<std::uint64_t> ulp_count;
   ulp_count.reserve(10);
-  for (int two_j1 = 0; two_j1 <= max_two_j; two_j1 += 2) {
-    for (int two_j2 = 0; two_j2 <= max_two_j; two_j2 += 2) {
-      for (int two_j3 = std::abs(two_j1 - two_j2); two_j3 <= two_j1 + two_j2; two_j3 += 2) {
-        for (int two_m1 = -two_j1; two_m1 <= two_j1; two_m1 += 2) {
-          for (int two_m2 = -two_j2; two_m2 <= two_j2; two_m2 += 2) {
+  for (int two_j1 = 0; two_j1 <= max_two_j; ++two_j1) {
+    for (int two_j2 = 0; two_j2 <= max_two_j; ++two_j2) {
+      for (int two_j3 = std::abs(two_j1 - two_j2); two_j3 <= two_j1 + two_j2; ++two_j3) {
+        for (int two_m1 = -two_j1; two_m1 <= two_j1; ++two_m1) {
+          for (int two_m2 = -two_j2; two_m2 <= two_j2; ++two_m2) {
             int two_m3 = -(two_m1 + two_m2);
             auto res_wigcpp = wigcpp::three_j(two_j1, two_j2, two_j3, two_m1, two_m2, two_m3);
             auto res_wigxjpf = wig3jj(two_j1, two_j2, two_j3, two_m1, two_m2, two_m3);
@@ -45,12 +45,12 @@ auto correctness_6j(int max_two_j) {
   wigcpp::ensure_global(max_two_j, 6);
   std::vector<std::uint64_t> ulp_count;
   ulp_count.reserve(10);
-  for (int two_j1 = 0; two_j1 <= max_two_j; two_j1 += 2) {
-    for (int two_j2 = 0; two_j2 <= max_two_j; two_j2 += 2) {
-      for (int two_j3 = 0; two_j3 <= max_two_j; two_j3 += 2) {
-        for (int two_j4 = 0; two_j4 <= max_two_j; two_j4 += 2) {
-          for (int two_j5 = 0; two_j5 <= max_two_j; two_j5 += 2) {
-            for (int two_j6 = 0; two_j6 <= max_two_j; two_j6 += 2) {
+  for (int two_j1 = 0; two_j1 <= max_two_j; ++two_j1) {
+    for (int two_j2 = 0; two_j2 <= max_two_j; ++two_j2) {
+      for (int two_j3 = 0; two_j3 <= max_two_j; ++two_j3) {
+        for (int two_j4 = 0; two_j4 <= max_two_j; ++two_j4) {
+          for (int two_j5 = 0; two_j5 <= max_two_j; ++two_j5) {
+            for (int two_j6 = 0; two_j6 <= max_two_j; ++two_j6) {
               auto res_wigcpp = wigcpp::six_j(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6);
               auto res_wigxjpf = wig6jj(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6);
               if (res_wigcpp == 0.0 && res_wigxjpf == 0.0) {
@@ -80,15 +80,15 @@ auto correctness_9j(int max_two_j) {
   wigcpp::ensure_global(max_two_j, 9);
   std::vector<std::uint64_t> ulp_count;
   ulp_count.reserve(10);
-  for (int two_j1 = 0; two_j1 < max_two_j; two_j1 += 2) {
-    for (int two_j2 = 0; two_j2 < max_two_j; two_j2 += 2) {
-      for (int two_j3 = 0; two_j3 < max_two_j; two_j3 += 2) {
-        for (int two_j4 = 0; two_j4 < max_two_j; two_j4 += 2) {
-          for (int two_j5 = 0; two_j5 < max_two_j; two_j5 += 2) {
-            for (int two_j6 = 0; two_j6 < max_two_j; two_j6 += 2) {
-              for (int two_j7 = 0; two_j7 < max_two_j; two_j7 += 2) {
-                for (int two_j8 = 0; two_j8 < max_two_j; two_j8 += 2) {
-                  for (int two_j9 = 0; two_j9 < max_two_j; two_j9 += 2) {
+  for (int two_j1 = 0; two_j1 < max_two_j; ++two_j1) {
+    for (int two_j2 = 0; two_j2 < max_two_j; ++two_j2) {
+      for (int two_j3 = 0; two_j3 < max_two_j; ++two_j3) {
+        for (int two_j4 = 0; two_j4 < max_two_j; ++two_j4) {
+          for (int two_j5 = 0; two_j5 < max_two_j; ++two_j5) {
+            for (int two_j6 = 0; two_j6 < max_two_j; ++two_j6) {
+              for (int two_j7 = 0; two_j7 < max_two_j; ++two_j7) {
+                for (int two_j8 = 0; two_j8 < max_two_j; ++two_j8) {
+                  for (int two_j9 = 0; two_j9 < max_two_j; ++two_j9) {
                     auto res_wigcpp =
                         wigcpp::nine_j(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6, two_j7, two_j8, two_j9);
                     auto res_wigxjpf = wig9jj(two_j1, two_j2, two_j3, two_j4, two_j5, two_j6, two_j7, two_j8, two_j9);
@@ -143,4 +143,7 @@ auto main(void) -> int {
   result = correctness_9j(12);
   std::cout << std::format("result of 9j correctness test:\n");
   print_ulp_stat(result);
+
+  wig_temp_free();
+  wig_table_free();
 }
